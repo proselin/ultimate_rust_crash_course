d55c767c1a365e4570f3295dcb34e3b2ddd842a4
Hung Nguyen Quoc <quochung9401@gmail.com>
2024-11-14 22:12:24 +0700

    =Update code

--- examples/hello/src/main.rs
+++ examples/hello/src/main.rs
@@ -4,4 +4,24 @@ fn main() {
     english::greet();
     spanish::greet();
     russian::greet();
+    mainada();
+}
+
+fn mainada() {
+    // let mut num = 5;
+    // let mut msg = "";
+    // if num == 5 {
+    //     msg = "five";
+    // }else if num == 6 {
+    //     msg = "six";
+    // }
+    let num = 6;
+    let msg = if num == 5 {
+        "fire"
+    }else if num == 6 {
+        "six"
+    } else {
+        "seven"
+    };
+    println!("{}", msg)
 }
